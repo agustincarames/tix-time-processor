940a7198f337576f92166cefd6d0c898468d10fa
Facundo Martinez <facu@spect.ro>
2017-04-16 20:47:02 -0300

    master: 	* Typo

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@
 if [ "${PROCESSOR_TYPE}" == "STANDALONE" ] ;
 then
     echo "Standalone processor started"
-    celery -A processor worker -B -s ${CELERY_BEAT_SCHEDULE_DIR}/celerybeat-schedule -l ${CELERY_LOG_LEVEL}
+    celery -A processor worker -B -s ${CELERY_BEAT_SCHEDULE_DIR}/celerybeat-scheduler -l ${CELERY_LOG_LEVEL}
 elif [ "${PROCESSOR_TYPE}" == "BEAT" ] ;
 then
     echo "Beat processor started"
-    celery -A processor beat -s ${CELERY_BEAT_SCHEDULE_DIR}/celerybeat-schedule -l ${CELERY_LOG_LEVEL}
+    celery -A processor beat -s ${CELERY_BEAT_SCHEDULE_DIR}/celerybeat-scheduler -l ${CELERY_LOG_LEVEL}
 elif [ "${PROCESSOR_TYPE}" == "WORKER" ] ;
 then
     echo "Worker processor started"
