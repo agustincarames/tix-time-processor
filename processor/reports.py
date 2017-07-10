96f6084963e1d87e3cd2127f52f1961fae75fb3e
Facundo Martinez <facu@spect.ro>
2017-07-10 00:22:18 -0300

    master: 	* Adding big refactor for testeability purposes

--- processor/reports.py
+++ processor/reports.py
@@ -9,6 +9,9 @@ import logging
 
 import struct
 
+import inflection
+import jsonschema
+
 OBSERVATIONS_PER_REPORT = 60
 MINIMUM_OBSERVATIONS_QTY = 1024
 MINIMUM_REPORTS_QTY = int(MINIMUM_OBSERVATIONS_QTY * 1.2 / OBSERVATIONS_PER_REPORT)
@@ -28,29 +31,227 @@ class ReportFieldTypes:
             self.name = name
             self.byte_size = byte_size
             self.struct_type = struct_type
+
+        def get_struct_representation(self):
+            return ReportFieldTypes.endian_type + self.struct_type
     endian_type = '>'
     Integer = ReportFieldType('int', 4, 'i')
-    Char = ReportFieldType('char', 1, 'b')
+    Char = ReportFieldType('char', 1, 'c')
     Long = ReportFieldType('long', 8, 'q')
 
 
-class ReportField:
+class FieldTranslation:
+    def __init__(self, original, translation, translator=None, reverse_translator=None):
+        self.original = original
+        self.translation = translation
+        self.translator = translator
+        self.reverse_translator = reverse_translator
+
+    def translate(self, field_value):
+        if self.translator is None:
+            return field_value
+        return self.translator(field_value)
+
+    def reverse_translate(self, field_value):
+        if self.reverse_translator is None:
+            return field_value
+        return self.reverse_translator(field_value)
+
+
+class Observation:
+    def __init__(self, day_timestamp, type_identifier, packet_size,
+                 initial_timestamp, reception_timestamp, sent_timestamp, final_timestamp):
+        self.day_timestamp = day_timestamp
+        self.type_identifier = type_identifier
+        self.packet_size = packet_size
+        self.initial_timestamp = initial_timestamp
+        self.reception_timestamp = reception_timestamp
+        self.sent_timestamp = sent_timestamp
+        self.final_timestamp = final_timestamp
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        return NotImplemented
+
+
+class SerializedObservationField:
     def __init__(self, name, report_field_type):
         self.name = name
         self.type = report_field_type
 
 
-class ReportLine:
+class SerializedObservation:
     fields = [
-        ReportField('day_timestamp', ReportFieldTypes.Long),
-        ReportField('type_identifier', ReportFieldTypes.Char),
-        ReportField('packet_size', ReportFieldTypes.Integer),
-        ReportField('initial_timestamp', ReportFieldTypes.Long),
-        ReportField('reception_timestamp', ReportFieldTypes.Long),
-        ReportField('sent_timestamp', ReportFieldTypes.Long),
-        ReportField('final_timestamp', ReportFieldTypes.Long),
+        # Timestamp in seconds since UNIX epoch of the time the packet was sent to the server
+        SerializedObservationField('day_timestamp', ReportFieldTypes.Long),
+        # Byte indicating if is a Long or Short packet
+        SerializedObservationField('type_identifier', ReportFieldTypes.Char),
+        # Size of the packet when it was sent to the server
+        SerializedObservationField('packet_size', ReportFieldTypes.Integer),
+        # The timestamp in nanoseconds since the start of the day in local time
+        # from when the packet was sent by the client to the server
+        SerializedObservationField('initial_timestamp', ReportFieldTypes.Long),
+        # The timestamp in nanoseconds since the start of the day in local time
+        # from when the packet was received by the server
+        SerializedObservationField('reception_timestamp', ReportFieldTypes.Long),
+        # The timestamp in nanoseconds since the start of the day in local time
+        # from when the packet was sent by the server to the client
+        SerializedObservationField('sent_timestamp', ReportFieldTypes.Long),
+        # The timestamp in nanoseconds since the start of the day in local time
+        # from when the packet was received by the client
+        SerializedObservationField('final_timestamp', ReportFieldTypes.Long),
     ]
     byte_size = sum([field.type.byte_size for field in fields])
+
+
+def serialize_observations(observations):
+    bytes_message = bytes()
+    for observation in observations:
+        observation_bytes = bytes()
+        for field in SerializedObservation.fields:
+            field_bytes = struct.pack(field.type.get_struct_representation(), getattr(observation, field.name))
+            observation_bytes = b''.join([observation_bytes, field_bytes])
+        bytes_message = b''.join([bytes_message, observation_bytes])
+    return base64.b64encode(bytes_message).decode()
+
+
+def deserialize_observations(message):
+    bytes_message = base64.b64decode(message)
+    observations = []
+    for message_index in range(0, len(bytes_message), SerializedObservation.byte_size):
+        line = bytes_message[message_index:message_index + SerializedObservation.byte_size]
+        observation_dict = {}
+        line_struct_format = ReportFieldTypes.endian_type
+        for field in SerializedObservation.fields:
+            line_struct_format += field.type.struct_type
+        line_tuple = struct.unpack(line_struct_format, line)
+        for field_index in range(len(SerializedObservation.fields)):
+            field = SerializedObservation.fields[field_index]
+            observation_dict[field.name] = line_tuple[field_index]
+        observations.append(Observation(**observation_dict))
+    return observations
+
+
+JSON_REPORT_SCHEMA = {
+    "type": "object",
+    "properties": {
+        "from": {
+            "anyOf": [
+                {"type": "string", "format": "ipv4"},
+                {"type": "string", "format": "ipv6"},
+                {"type": "string", "format": "hostname"}
+            ]
+        },
+        "to": {
+            "anyOf": [
+                {"type": "string", "format": "ipv4"},
+                {"type": "string", "format": "ipv6"},
+                {"type": "string", "format": "hostname"}
+            ]
+        },
+        "type": {
+            "type": "string",
+            "enum": ["S", "L"]
+        },
+        "initialTimestamp": {"type": "integer"},
+        "receivedTimestamp": {"type": "integer"},
+        "sentTimestamp": {"type": "integer"},
+        "finalTimestamp": {"type": "integer"},
+        "publicKey": {"type": "string"},
+        "message": {"type": "string"},
+        "signature": {"type": "string"},
+        "userId": {"type": "integer"},
+        "installationId": {"type": "integer"}
+    },
+    "required": [
+        "from", "to", "type",
+        "initialTimestamp", "receivedTimestamp", "sentTimestamp", "finalTimestamp",
+        "publicKey", "message", "signature",
+        "userId", "installationId"
+    ]
+}
+JSON_FIELDS_TRANSLATIONS = [
+    FieldTranslation("from", "from_dir"),
+    FieldTranslation("to", "to_dir"),
+    FieldTranslation("type", "packet_type"),
+    FieldTranslation("message", "observations", deserialize_observations, serialize_observations)
+]
+
+
+class ReportJSONEncoder(json.JSONEncoder):
+    def object_to_json(self, report_object):
+        report_dict = report_object.__dict__.copy()
+        for field_translation in JSON_FIELDS_TRANSLATIONS:
+            field_value = report_dict.pop(field_translation.translation)
+            report_dict[field_translation.original] = field_translation.reverse_translate(field_value)
+        report_dict_fields = report_dict.keys()
+        for field in report_dict_fields:
+            inflexed_key = inflection.camelize(field, False)
+            report_dict[inflexed_key] = report_dict.pop(field)
+        return report_dict
+
+    def default(self, obj):
+        if isinstance(obj, Report):
+            json_dict = self.object_to_json(obj)
+        else:
+            json_dict = json.JSONEncoder.default(self, obj)
+        return json_dict
+
+
+class ReportJSONDecoder(json.JSONDecoder):
+    def json_to_object(self, json_dict):
+        json_dict_keys = json_dict.keys()
+        for key in json_dict_keys:
+            new_key = inflection.underscore(key)
+            json_dict[new_key] = json_dict.pop(key)
+        for field_translation in JSON_FIELDS_TRANSLATIONS:
+            if field_translation.original in json_dict.keys():
+                field_value = json_dict.pop(field_translation.original)
+                json_dict[field_translation.translation] = field_translation.translate(field_value)
+        return Report(**json_dict)
+
+    def dict_to_object(self, d):
+        try:
+            jsonschema.validate(d, JSON_REPORT_SCHEMA)
+            inst = self.json_to_object(d)
+        except jsonschema.ValidationError:
+            inst = d
+        return inst
+
+    def __init__(self):
+        json.JSONDecoder.__init__(self, object_hook=self.dict_to_object)
+
+
+class Report:
+    def __init__(self,
+                 from_dir, to_dir, packet_type,
+                 initial_timestamp, received_timestamp, sent_timestamp, final_timestamp,
+                 public_key, observations, signature,
+                 user_id, installation_id):
+        self.from_dir = from_dir
+        self.to_dir = to_dir
+        self.packet_type = packet_type
+        self.initial_timestamp = initial_timestamp
+        self.received_timestamp = received_timestamp
+        self.sent_timestamp = sent_timestamp
+        self.final_timestamp = final_timestamp
+        self.public_key = public_key
+        self.observations = observations
+        self.signature = signature
+        self.user_id = user_id
+        self.installation_id = installation_id
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        return NotImplemented
+
+
+class ReportFileMetadata:
+    def __init__(self, file_path, file_name):
+        self.file_path = file_path
+        self.file_name = file_name
 
 
 def get_reports_files(installation_dir_path):
@@ -59,7 +260,7 @@ def get_reports_files(installation_dir_path):
         if file_name.endswith('.json'):
             file_path = join(installation_dir_path, file_name)
             if isfile(file_path) and not islink(file_path):
-                reports_files.append((file_path, file_name))
+                reports_files.append(ReportFileMetadata(file_path, file_name))
     return reports_files
 
 
@@ -75,7 +276,7 @@ def get_back_up_reports(installation_dir_path, reports_quantity):
         if file_name.endswith('.json'):
             file_path = join(back_up_reports_path, file_name)
             if isfile(file_path) and not islink(file_path):
-                back_up_reports.append((file_path, file_name))
+                back_up_reports.append(ReportFileMetadata(file_path, file_name))
     return back_up_reports
 
 
@@ -87,39 +288,26 @@ def clean_back_up_dir(installation_dir_path):
             unlink(file_path)
 
 
-def get_report_first_timestamp(report_file):
-    with open(report_file[0]) as fp:
-        report = json.load(fp)
-        message_bytes = base64.b64decode(report['message'])
-        field = ReportLine.fields[0]
-        struct_format = ReportFieldTypes.endian_type + field.type.struct_type
-        bytes_to_read = field.type.byte_size
-        first_timestamp = struct.unpack(struct_format, message_bytes[:bytes_to_read])[0]
-        return first_timestamp
+def get_report_object(report_file):
+    with open(report_file.file_path) as fp:
+        report = json.load(fp, cls=ReportJSONDecoder)
+    return report
 
 
-def get_report_last_timestamp(report_file):
-    with open(report_file[0]) as fp:
-        report = json.load(fp)
-        message_bytes = base64.b64decode(report['message'])
-        last_line = message_bytes[-ReportLine.byte_size:]
-        field = ReportLine.fields[0]
-        struct_format = ReportFieldTypes.endian_type + field.type.struct_type
-        bytes_to_read = field.type.byte_size
-        last_timestamp = struct.unpack(struct_format, last_line[:bytes_to_read])[0]
-        return last_timestamp
+def get_report_object_message_bytes(report_object):
+    message_bytes = base64.b64decode(report_object.message)
+    return message_bytes
 
 
 def get_report_gap(report_file):
-    first_timestamp = get_report_first_timestamp(report_file)
-    last_timestamp = get_report_last_timestamp(report_file)
-    return last_timestamp - first_timestamp
+    report = get_report_object(report_file)
+    return report.observations[-1].day_timestamp - report.observations[0].day_timestamp
 
 
 def get_gap_between_reports(second_report_file, first_report_file):
-    first_report_first_timestamp = get_report_first_timestamp(first_report_file)
-    second_report_first_timestamp = get_report_first_timestamp(second_report_file)
-    return second_report_first_timestamp - first_report_first_timestamp
+    second_report = get_report_object(second_report_file)
+    first_report = get_report_object(first_report_file)
+    return second_report.observations[0].day_timestamp - first_report.observations[0].day_timestamp
 
 
 def max_gap_in_reports(reports_files):
@@ -143,24 +331,7 @@ def delete_files_before_last_gap_occurrence(gap, reports_files):
                 reports_files_to_delete = reports_files[:index]
                 break
     for report_file in reports_files_to_delete:
-        unlink(report_file[0])
-
-
-def extract_observations_from_report_message(report_message):
-    bytes_message = base64.b85decode(report_message)
-    observations = []
-    for message_index in range(0, len(bytes_message), ReportLine.byte_size):
-        line = bytes_message[message_index:message_index+ReportLine.byte_size]
-        observation = {}
-        line_struct_format = ReportFieldTypes.endian_type
-        for field in ReportLine.fields:
-            line_struct_format += field.type.struct_type
-        line_tuple = struct.unpack(line_struct_format, line)
-        for field_index in range(len(ReportLine.fields)):
-            field = ReportLine.fields[field_index]
-            observation[field.name] = line_tuple[field_index]
-        observations.append(observation)
-    return observations
+        unlink(report_file.file_path)
 
 
 def get_processable_report_files(installation_dir_path):
@@ -192,16 +363,14 @@ def get_processable_report_files(installation_dir_path):
     return reports_files
 
 
-def extract_processable_data(reports_files):
+def extract_processable_data(reports_files_metadata):
     data_per_ip = {}
-    for report_file in reports_files:
-        with open(report_file[0]) as report_pf:
-            report = json.load(report_pf)
-            ip = report['from']
-            if ip not in data_per_ip:
-                data_per_ip[ip] = list()
-            report_observations = extract_observations_from_report_message(report['message'])
-            data_per_ip[ip].extend(report_observations)
+    for report_file_metadata in reports_files_metadata:
+        report_object = get_report_object(report_file_metadata)
+        ip = report_object.from_dir
+        if ip not in data_per_ip:
+            data_per_ip[ip] = list()
+        data_per_ip[ip].extend(report_object.observations)
     for ip, observations in data_per_ip.items():
         if len(observations) >= MINIMUM_OBSERVATIONS_QTY:
             return ip, observations
@@ -216,7 +385,7 @@ def get_data(installation_dir_path):
     return data
 
 
-def failed_results_empty(installation_dir_path):
+def failed_results_dir_is_empty(installation_dir_path):
     failed_results_dir_path = join(installation_dir_path, FAILED_REPORTS_DIR_NAME)
     return not exists(failed_results_dir_path) or len(listdir(failed_results_dir_path)) == 0
 
---
+++ tests/test_reports.py
@@ -0,0 +1,81 @@
+import base64
+import json
+import random
+import socket
+import struct
+import unittest
+
+import datetime
+
+import jsonschema
+
+from processor import reports
+
+
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 80))
+    ip = '{ip}:{port}'.format(ip=s.getsockname()[0], port=s.getsockname()[1])
+    s.close()
+    return ip
+
+
+class TestReports(unittest.TestCase):
+    DEFAULT_REPORT_DELTA = datetime.timedelta(minutes=1)
+    DEFAULT_OBSERVATIONS_DELTA = datetime.timedelta(seconds=1)
+    USER_ID = random.randint(1, 10)
+    INSTALLATION_ID = random.randint(1, 10)
+
+    @staticmethod
+    def generateObservations():
+        start_time = datetime.datetime.now()
+        end_time = start_time + datetime.timedelta(minutes=1)
+        observations_time_delta = datetime.timedelta(seconds=1)
+        current_time = start_time
+        observations = []
+        while current_time < end_time:
+            day_timestamp = int(current_time.timestamp())
+            type_identifier = 'S'.encode()
+            packet_size = 64
+            start_of_day = current_time.replace(hour=0, minute=0, second=0, microsecond=0)
+            nanos_in_a_day = 24 * 60 * 60 * (10 ** 9)
+            initial_timestamp = int((current_time - start_of_day).total_seconds() * 10 ** 9) % nanos_in_a_day
+            transmission_time = random.randint(1, 10 ** 9)
+            reception_timestamp = (initial_timestamp + transmission_time) % nanos_in_a_day
+            processing_time = random.randint(1, 10 ** 6)
+            sent_timestamp = (reception_timestamp + processing_time) % nanos_in_a_day
+            transmission_time = random.randint(1, 10 ** 9)
+            final_timestamp = (sent_timestamp + transmission_time) % nanos_in_a_day
+            observation = reports.Observation(day_timestamp, type_identifier, packet_size,
+                                              initial_timestamp, reception_timestamp, sent_timestamp, final_timestamp)
+            observations.append(observation)
+            current_time += observations_time_delta
+        return observations
+
+    def generateReport(self):
+        from_dir = get_ip_address()
+        to_dir = '8.8.8.8:4500'
+        packet_type = 'S'
+        initial_timestamp = received_timestamp = sent_timestamp = final_timestamp = 0
+        public_key = signature = 'a'
+        observations = self.generateObservations()
+        return reports.Report(from_dir=from_dir,
+                              to_dir=to_dir,
+                              packet_type=packet_type,
+                              initial_timestamp=initial_timestamp,
+                              received_timestamp=received_timestamp,
+                              sent_timestamp=sent_timestamp,
+                              final_timestamp=final_timestamp,
+                              public_key=public_key,
+                              observations=observations,
+                              signature=signature,
+                              user_id=self.USER_ID,
+                              installation_id=self.INSTALLATION_ID)
+
+    def testJSONCoDec(self):
+        report = self.generateReport()
+        json_report_string = json.dumps(report, cls=reports.ReportJSONEncoder)
+        other_report = json.loads(json_report_string, cls=reports.ReportJSONDecoder)
+        self.assertEqual(report, other_report)
+        naive_json_report = json.loads(json_report_string)
+        jsonschema.validate(naive_json_report, reports.JSON_REPORT_SCHEMA)
