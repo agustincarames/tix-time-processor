793ea3516a69bd2b5264ca772a4e5a7ddcf487be
Facundo Martinez <facu@spect.ro>
2017-09-09 14:20:46 -0300

    master: 	* Because of the logic change in the report gathering, this is a more meaningful data to use

--- processor/analysis.py
+++ processor/analysis.py
@@ -258,7 +258,7 @@ def process_observations(observations):
                                                        downstream_hurst=downstream_hurst,
                                                        phi_function=phi_function)
     return {
-        'timestamp': meaningful_observations[0].day_timestamp,
+        'timestamp': meaningful_observations[-1].day_timestamp,
         'upstream': {
             'usage': upstream_usage,
             'quality': upstream_quality,
